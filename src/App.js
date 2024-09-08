6edcc9528090c24b6545416f63484c9bde2dd461
Skepticrob1 <104695294+Skepticrob1@users.noreply.github.com>
2024-09-08 15:21:57 +0800

    Added 404 Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from './views/dashboard/dashboard.component';
 import Settings from './views/settings/settings.component';
+import Errorpage from "./views/404/404.component";
 
 import {
   BrowserRouter as Router,
@@ -15,7 +16,7 @@ function App() {
     <Routes>
       <Route exact path="/dashboard" element={<Dashboard />} /> //Render Dashboard if route http://localhost:3000/dashboard
       <Route exact path="/Settings" element={<Settings />} /> //Render Dashboard if route http://localhost:3000/settings
-      <Route path="*" element={<Dashboard />} /> //Element to be render if path cannot be found / 404
+      <Route path="*" element={<Errorpage />} /> //Element to be render if path cannot be found / 404
     </Routes>
   );
 }
